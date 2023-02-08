e1c62281e43fe140786cb16f0d09ce8a0993a7a0
kevinkkimutai <kiruikevinkim@gmail.com>
2023-02-08 15:43:32 +0300

    Assignment completed

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,25 @@
 # Make your shoe class here!
+require 'pry'
+class Shoe
+    attr_accessor :brand
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+    def initialize(brand)
+        @condition = condition
+        @brand = brand
+        @color = color
+        @size = size
+        @material = material
+     
+    end
+    def cobble 
+        puts "Your shoe is as good as new!"
+      @condition = "new"
+    end
+
+
+
+
+end
